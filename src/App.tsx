27ad885dd5dce9b7e783040cfd35bd403cf6dd52
Siwek9 <72036867+Siwek9@users.noreply.github.com>
2024-10-31 10:19:12 +0100

    Add first fetch
    😂😂

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { LoginPage } from './routes/LoginPage';
 import { Panel } from './routes/Panel';
+import { Profile } from './routes/Profile';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/panel" element={<Panel />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   )
 }
--- src/routes/LoginPage.tsx
+++ src/routes/LoginPage.tsx
@@ -1,13 +1,37 @@
-import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function LoginPage() {
-    const navigate = useNavigate();
-    return <>
-        <form action="">
-            Login: <input type="text" />
-            Haslo: <input type="password" name="" id="" />
-        </form>
-        Nie masz jeszcze konta? Utwórz jedno
-        <button onClick={() => { navigate("panel") }}>Zaloguj się</button>
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/check-autorization", {
+      method: "POST",
+      body: JSON.stringify({ test: "example" }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((content) => setValue(JSON.stringify(content)));
+  }, []);
+
+  const navigate = useNavigate();
+  return (
+    <>
+      <form action="">
+        Login: <input type="text" />
+        Haslo: <input type="password" name="" id="" />
+      </form>
+      Nie masz jeszcze konta? Utwórz jedno
+      <button
+        onClick={() => {
+          navigate("panel");
+        }}
+      >
+        Zaloguj się
+      </button>
+      <div>{value}</div>
     </>
+  );
 }
