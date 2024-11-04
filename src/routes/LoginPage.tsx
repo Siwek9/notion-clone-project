b74254b668ecba404b465960eb0a69e51e6c8609
Siwek9 <72036867+Siwek9@users.noreply.github.com>
2024-11-04 19:49:29 +0100

    Add client-server communication for log in

--- src/routes/LoginPage.tsx
+++ src/routes/LoginPage.tsx
@@ -1,70 +1,97 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function LoginPage() {
-  const [value, setValue] = useState("");
+    const [loginOrEmail, setLoginOrEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const [passValue, setPassValue] = useState<string>('');
-  const [showPass, setShowPass] = useState<boolean>(false);
+    const [showPass, setShowPass] = useState(false);
 
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/check-autorization", {
-      method: "POST",
-      body: JSON.stringify({ test: "example" }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((content) => setValue(JSON.stringify(content)));
-  }, []);
+    // useEffect(() => {
+    //     fetch("http://127.0.0.1:8000/register", {
+    //         method: "POST",
+    //         body: JSON.stringify({ test: "example" }),
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //     })
+    //         .then((res) => res.json())
+    //         .then((content) => setValue(JSON.stringify(content)));
+    // }, []);
 
-  const navigate = useNavigate();
-  return (
-    <>
-      <div id="container">
-        <form action="" className="form">
-          <h2>Zaloguj się</h2>
-          
-          <input type="text" id="nameoremail" name="nameoremail" placeholder="Nazwa lub Email"/> <br/>
-          
-          <div className="passwordBox">
-                <input type={showPass ? 'text' : 'password'}
-                id="password"
-                name="password"
-                placeholder="Hasło"
-                value={passValue}
-                onChange={(e) => setPassValue(e.target.value)}
-                />
-                <span
-                  className="showToggle"
-                  onClick={() => {
-                    setShowPass(!showPass);
-                  }}
-                  >
-                  <img src={showPass ? "/src/assets/eye_closed.svg" : "/src/assets/eye.svg"} 
-                  alt={showPass ? "Ukryj" : "Pokaż"}  />
-                </span> <br/>
-          </div>
-        <button
-          onClick={() => {
-            navigate("panel");
-          }}
-        >
-        Zaloguj się
-        </button>
-        <div>{value}</div>
-        </form>
-        </div>
-        <br/>
-        <span className="nav"
-          onClick={() => {
-            navigate("register");
-          }}
-        >
-          Nie posiadasz jeszcze konta? Utwórz je
-        </span>
-      
-    </>
-  );
+    const navigate = useNavigate();
+    return (
+        <>
+            <div id="container">
+                <form
+                    className="form"
+                    method="post"
+                    action="/"
+                    onSubmit={(event) => {
+                        const formData = {
+                            loginOrEmail: loginOrEmail,
+                            password: password,
+                        };
+
+                        fetch("http://127.0.0.1:8000/log-in", {
+                            method: "POST",
+                            body: JSON.stringify(formData),
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                        })
+                            .then((res) => res.json())
+                            .then((content) => console.log(content));
+                        event.preventDefault();
+                    }}
+                >
+                    <h2>Zaloguj się</h2>
+                    <input
+                        type="text"
+                        id="username"
+                        name="loginOrEmail"
+                        placeholder="Nazwa lub Email"
+                        value={loginOrEmail}
+                        onChange={(e) => setLoginOrEmail(e.target.value)}
+                    />
+                    <br />
+                    <div className="passwordBox">
+                        <input
+                            type={showPass ? "text" : "password"}
+                            id="password"
+                            name="password"
+                            placeholder="Hasło"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <span
+                            className="showToggle"
+                            onClick={() => {
+                                setShowPass(!showPass);
+                            }}
+                        >
+                            <img
+                                src={
+                                    showPass
+                                        ? "/src/assets/eye_closed.svg"
+                                        : "/src/assets/eye.svg"
+                                }
+                                alt={showPass ? "Ukryj" : "Pokaż"}
+                            />
+                        </span>
+                        <br />
+                    </div>
+                    <button>Zaloguj się</button>
+                </form>
+            </div>
+            <br />
+            <button
+                onClick={() => {
+                    navigate("register");
+                }}
+            >
+                Nie posiadasz jeszcze konta? Utwórz je
+            </button>
+        </>
+    );
 }
--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,5 +1,3 @@
 export function Profile() {
-    return <>
-        Marek Marucha
-    </>
+    return <>Marek Marucha</>;
 }
